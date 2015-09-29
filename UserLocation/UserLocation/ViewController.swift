d652cf06b556018590007ed6f47c9c4831186cd1
John Bogil <johncbogil@gmail.com>
2015-09-29 15:21:52 -0400

    location manager in swift

---
+++ UserLocation/UserLocation/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  UserLocation
+//
+//  Created by John Bogil on 9/29/15.
+//  Copyright © 2015 John Bogil. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
+}
